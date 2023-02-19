345ee46ca6a3e13dd5d397ba6b6b691afadae182
LuoXingyue <lyjh123abc@163.com>
2023-02-19 23:25:48 +0800

    2023-02-19 stateCard.wxss完善

--- app/pages/stateCard/stateCard.js
+++ app/pages/stateCard/stateCard.js
@@ -4,19 +4,18 @@ Component({
 			type: String,
 			value: '/../static/default_image.png'
 		},
-		params: {
-			type: Array,
-			value: []
-		},
 		imageDescription: {
 			type: String,
 			value: '暂无图片'
 		},
-		defaultData:{
+		params: {			// 传入的数据
+			type: Array,
+			value: []
+		},
+		defaultData:{  		// 站点是否存在数据
 			type: Boolean,
 			value: false
 		},
-		
 	},
 	data:{
 		
